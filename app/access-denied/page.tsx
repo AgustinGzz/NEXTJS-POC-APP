56fd2f4f1ffc7f2efe9fd908edf8df9d6e427bb0
AgustinGzz <tinyaelito@gmail.com>
2023-11-19 04:22:11 -0600

    style access denied

--- app/access-denied/page.tsx
+++ app/access-denied/page.tsx
@@ -1,5 +1,7 @@
 const AccessDenied: React.FC = () => {
-  return <div>access denied!</div>;
+  return (
+    <div className='block p-6 px-10 bg-red-600 rounded-2xl'>Access Denied!</div>
+  );
 };
 
 export default AccessDenied;
